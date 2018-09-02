5677af9585c8ca229c09b521bff93cc773e6ad2e
Akshay Borgave <borgave.akshay@gmail.com>
2018-09-03 01:13:38 +0530

    - presentation DI implementation initial commit

--- app/src/main/java/news/agoda/com/sample/dependencies/Components/AppComponent.kt
+++ app/src/main/java/news/agoda/com/sample/dependencies/Components/AppComponent.kt
@@ -1,8 +1,17 @@
-package news.agoda.com.sample.dependencies.Components
+package news.agoda.com.sample.dependencies.components
 
 import dagger.Component
+import news.agoda.com.sample.Utils.NewsViewModelProviderFactory
+import news.agoda.com.sample.adapters.NewsAdapter
+import news.agoda.com.sample.dependencies.modules.AppModule
+import news.agoda.com.sample.dependencies.scopes.AppScope
+import news.agoda.com.sample.observables.NewsScreenObservable
 
-@Component
+@AppScope
+@Component(modules = [AppModule::class], dependencies = [NetworkComponent::class])
 interface AppComponent{
 
+    fun getNewsScreenObservable(): NewsScreenObservable
+    fun getNewsAdapter(): NewsAdapter
+    fun getNewsViewModelFactory(): NewsViewModelProviderFactory
 }
